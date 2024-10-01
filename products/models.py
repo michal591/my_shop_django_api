0c792b13d646374a0cf029713e80a0fbbcd45398
unknown <m0583266591@gmail.com>
2024-10-01 18:13:51 +0300

    add image

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=255)
     popularity = models.IntegerField()
@@ -7,11 +8,13 @@ class Category(models.Model):
     def __str__(self):
         return f"{self.name} - {self.popularity}"
 
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ManyToManyField(Category,related_name="products")
+    category = models.ManyToManyField(Category, related_name="products")
+    image = models.ImageField(null=True, blank=True, default="/placeholder.png")
 
     def __str__(self):
         return f"{self.name} - {self.price}"
